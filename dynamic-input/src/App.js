74c1ca987fcce5a0ed449ae618ab9c176d07fb22
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-07-13 00:17:01 +0800

    2 way binding in class achieved

---
+++ dynamic-input/src/App.js
@@ -0,0 +1,73 @@
+import React from "react"
+import {v4 as uuidv4} from "uuid"
+import DynamicForm from "./DynamicForm"
+
+export default function App() {
+  const [inviteMembers, setInviteMembers] = React.useState([
+    {
+      name: "",
+      email: "",
+      id: uuidv4(),
+    }
+  ])
+
+  const addMemberRow = () => {
+    // duplicate existing array
+    let _inviteMembers = [...inviteMembers]
+    // add new object
+    _inviteMembers.push({
+      name: "",
+      email: "",
+      id: uuidv4()
+    })
+    // set state
+    setInviteMembers(_inviteMembers)
+  }
+
+  const removeMemberRow = (id) => {
+    // duplicate existing array
+    let _inviteMembers = [...inviteMembers]
+    // add new object
+    _inviteMembers = _inviteMembers.filter(member=>member.id!==id)
+    // set state
+    setInviteMembers(_inviteMembers)
+  }
+
+  const handleMemberChange=(id, event)=>{
+    // find index to be changed
+    const index=inviteMembers.findIndex(m=>m.id===id)
+    let _inviteMembers = [...inviteMembers] 
+    _inviteMembers[index][event.target.name] = event.target.value
+    // set state
+    setInviteMembers(_inviteMembers)
+  }  
+
+  return (
+    <React.Fragment>
+      <div>
+        {inviteMembers.map((member) => (
+          <React.Fragment>
+            <div key={member.id}>
+              <div>
+                <label>Name</label>
+                <input name="name" type="text" onChange={(e)=>handleMemberChange(member.id, e)} />
+              </div>
+              <div>
+                <label>Email</label>
+                <input name="email" type="text" onChange={(e)=>handleMemberChange(member.id, e)}/>
+              </div>
+              <button onClick={addMemberRow}>Add New</button>
+              {inviteMembers.length>1 && (
+                <button onClick={()=>removeMemberRow(member.id)}>Remove Entry</button>
+              )}
+            </div>
+
+          </React.Fragment>
+        ))}
+      </div>
+
+      <h1>Dynamic form clone</h1>
+      <DynamicForm/>
+    </React.Fragment>
+  );
+}
---
+++ dynamic-input/src/DynamicForm.js
@@ -0,0 +1,76 @@
+import React from "react"
+import { v4 as uuidv4 } from "uuid"
+
+export default class DynamicForm extends React.Component {
+    state = {
+        inviteMembers: [
+            {
+                name: "",
+                email: "",
+                id: uuidv4()
+            }
+        ]
+    }
+
+    addMemberRow = () => {
+        // duplicate existing array
+        let _inviteMembers = [...this.state.inviteMembers];
+        // add new object
+        _inviteMembers.push({
+            name: "",
+            email: "",
+            id: uuidv4()
+        })
+        // set state
+        this.setState({
+            inviteMembers: _inviteMembers
+        })
+    }
+
+    removeMemberRow = (id) => {
+        // duplicate existing array
+        let _inviteMembers = [...this.state.inviteMembers];
+        // filter current id
+        _inviteMembers = _inviteMembers.filter(member=>member.id!==id)
+        // set state
+        this.setState({
+            inviteMembers: _inviteMembers
+        })
+    }
+
+    handleFormChange = (id, e) => {
+        // find index to be changed
+        let index = this.state.inviteMembers.findIndex(m => m.id === id)
+        let _inviteMembers = [...this.state.inviteMembers]
+        _inviteMembers[index][e.target.name] = e.target.value
+        // set state
+        this.setState({
+            inviteMembers: _inviteMembers
+        })
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                {this.state.inviteMembers.map((member) => (
+                    <React.Fragment>
+                        <div key={member.id}>
+                            <div>
+                                <label>Name</label>
+                                <input name="name" type="text" value={member.name} onChange={(e)=>this.handleFormChange(member.id, e)}/>
+                            </div>
+                            <div>
+                                <label>Email</label>
+                                <input name="email" type="text" value={member.email} onChange={(e)=>this.handleFormChange(member.id, e)} />
+                            </div>
+                            <button onClick={this.addMemberRow}>Add New</button>
+                            {this.state.inviteMembers.length>1 && (
+                                <button onClick={()=>this.removeMemberRow(member.id)}>Remove Entry</button>
+                            )}
+                        </div>
+                    </React.Fragment>
+                ))}
+            </React.Fragment>
+        );
+    }
+}
